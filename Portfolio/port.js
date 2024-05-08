fe4e891c17df01728c73595512a3a5ddc3d5f575
Ravindra <ravidharme99@gmail.com>
2024-05-08 11:17:39 +0530

    portfolio

---
+++ Portfolio/port.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+// document.getElementById("contact-form").addEventListener("submit", function(event) {
+//     event.preventDefault(); // Prevents the default form submission behavior
+//     // You can add your form submission logic here, like sending an email or storing data
+//     alert("Form submitted!");
+// });
+// function doPost(e) {
+//     var sheet = SpreadsheetApp.openById('1iBwwAT60IYfE5HEST1y_47vOfly5kzg69ihVUtM_mFA').getActiveSheet();
+//     var rowData = [];
+//     rowData.push(new Date()); // Timestamp
+//     rowData.push(e.parameter.name);
+//     rowData.push(e.parameter.email);
+//     rowData.push(e.parameter.message);
+//     sheet.appendRow(rowData);
+//     return ContentService.createTextOutput("Form submitted successfully").setMimeType(ContentService.MimeType.TEXT);
+//   }
+  
